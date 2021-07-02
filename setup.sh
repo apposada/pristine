39b4601b53ed7088862c68b51428f87043556055
Alberto Perez-Posada <ap.posada1@gmail.com>
2021-07-02 09:35:22 +0200

    added preliminary setup script; added sub-scripts for database retrieval

--- setup.sh
+++ setup.sh
@@ -1,35 +1,24 @@
+#!/bin/bash
+
 source /home/aperpos/programs/miniconda3/bin/activate
 conda install -c conda-forge mamba
-mamba create -n pristine_venv -c bioconda -c conda-forge fastqc rcorrector trim-galore bbmap bowtie2 trinity busco transdecoder
+mamba create -n pristine_venv -c bioconda -c conda-forge fastqc rcorrector trim-galore bbmap bowtie2 trinity transdecoder hmmer busco
 
 conda activate pristine_venv
 
 echo -e "Downloading assets:\n"
+
 echo -e "Downloading SILVA database, concatenating and grouping, this might take a while..."
 cd ${PATH_TO_PRISTINE_ENV}/assets/SILVA
 . ./get_silva.sh
-	#wget  https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA_138.1_LSUParc_tax_silva.fasta.gz
-	#wget  https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA_138.1_SSUParc_tax_silva.fasta.gz
-	#zcat *fasta.gz > SILVA.fasta
-	#rm SILVA_138.1_LSUParc_tax_silva.fasta.gz
-	#rm SILVA_138.1_SSUParc_tax_silva.fasta.gz
-	#echo -e "Indexing the SILVA database...""
-	#source /home/aperpos/programs/miniconda3/bin/activate
-	#conda activate pristine_venv
-	#bowtie2-build -i bvlablabalabla etc.
-
 cd ${PATH_TO_PRISTINE_ENV}/
-echo -e "SILVA database downloaded.\n"
-
-
+echo -e "SILVA database set up.\n"
 
 echo -e "Downloading Pfam database...\n"
 cd ${PATH_TO_PRISTINE_ENV}/assets/pfam/
 . ./get_pfam.sh
-	# wget https:/pfam
 cd ${PATH_TO_PRISTINE_ENV}/
 echo -e "Pfam-A.hmm database downloaded.\n"
-
 
 
 echo "Downloading TranscriptomeAssemblyTools ..."
@@ -39,15 +28,11 @@ git clone https://github.com/harvardinformatics/TranscriptomeAssemblyTools
 TAToolsDir="$(dir ${PATH_TO_PRISTINE_ENV}/TranscriptomeAssemblyTools )" 
 echo
 echo -e "TranscriptomeAssemblyTools succesfully cloned and available at :\n$TAToolsDir \n\n"
-echo "Please add this directory to your PATH to make it available for pristine"
-
-# mamba install -c bioconda -c conda-forge fastqc
-# mamba install -c rcorrector
-# install transcriptomeassemblytools goes here
-# mamba install -c bioconda -c conda-forge trim-galore
-# mamba install -c bioconda -c conda-forge bbmap
-# mamba install -c bioconda -c conda-forge bowtie2
-# mamba install -c bioconda -c conda-forge trinity
-# mamba install -c bioconda -c conda-forge busco
-# mamba install -c bioconda -c conda-forge transdecoder
+echo "Please add this directory to your PATH variable to make it available for pristine. You can do so by:"
+echo "Going to your home directory"
+echo "Open up the .bash_profile , or the .profile file"
+echo "look for the line where 'PATH=' is defined"
+echo "add: '$TAToolsDir:' right after the equal sign"
+echo "save changes and exit"
+echo "type: 'source ./bash_profile' (or /.profile, depends on what you have)"
 
